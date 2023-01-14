0ed555d73737748eedfc372c0198e42c9c102ad3
ivybart <ivybart54@gmail.com>
2023-01-14 18:54:51 +0300

    my first commit

---
+++ Frontend/JS/script.js
@@ -0,0 +1,51 @@
+$(document).ready(function(){ 
+ 
+    $(".primaryContained").on('click', function(){
+    $(".comment").addClass("commentClicked");
+  });//end click
+  $("textarea").on('keyup.enter', function(){
+    $(".comment").addClass("commentClicked");
+  });//end keyup
+  });//End Function
+ 
+new Vue({
+    el: "#app",
+    data:{
+       title: 'Add a comment',
+      newItem: '',
+      item: [],
+    },
+    methods:{
+      addItem  (){
+      this.item.push(this.newItem);
+        this.newItem = "";
+      }
+  }
+ 
+  });
+/* interactivity for tabs*/
+
+function openPage(pageName, elmnt, color) {
+  // Hide all elements with class="tabcontent" by default */
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  // Remove the background color of all tablinks/buttons
+  tablinks = document.getElementsByClassName("tablink");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].style.backgroundColor = "";
+  }
+
+  // Show the specific tab content
+  document.getElementById(pageName).style.display = "block";
+
+  // Add the specific color to the button used to open the tab content
+  elmnt.style.backgroundColor = color;
+}
+
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
+
